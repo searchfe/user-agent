a7e8e97c1be5cc416f27aef2b00a43db518ddcd4
zifeiyu <biyingshuai@gmail.com>
2020-09-15 19:27:43 +0800

    feat: add index.d.ts file

---
+++ src/index.d.ts
@@ -0,0 +1,39 @@
+interface factory {
+    isAndroid(): boolean;
+    isIOS(): boolean;
+    isWinPhone(): boolean;
+
+    iOSVersion(): [number, number] | [];
+    appleWebkitVersion(): Array<number>;
+    baiduBoxVersion(): Array<number>;
+    secrVersion(): Array<number>;
+    getChromeVersion(): Array<number>;
+    androidVersion(): Array<number>;
+
+    isBaiduBox(): boolean;
+    isBaiduBoxLite(): boolean;
+    isBaiduBoxJisu(): boolean;
+    isBaiduBoxVision(): boolean;
+    isQQ(): boolean;
+    isQQApp(): boolean;
+    isWeixinApp(): boolean;
+    isQQBrowser(): boolean;
+    isBaiduBrowser(): boolean;
+    isSearchCraft(): boolean;
+    isUC(): boolean;
+    isChromeDesktop(): boolean;
+    isChromeMobile(): boolean;
+    isCriOS(): boolean;
+    isSogouMobile(): boolean;
+    isMiuiBrowser(): boolean;
+    isHUAWEIBrowser(): boolean;
+    isMZBrowser(): boolean;
+    isWKWebview(): boolean;
+    isUIWebview(): boolean;
+    isXcxApp(): boolean;
+    use(ua: string): factory
+}
+
+declare const mod: factory;
+
+export = mod
